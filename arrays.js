1dad639cbbc5f4aa63700746fc7432351f7ad167
Jane Amero <janie.amero@gmail.com>
2018-10-08 22:55:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(chocolateBars, 'x'){
-   array == ['x', ...chocolateBars];
+   array = ['x', ...chocolateBars];
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, x){
