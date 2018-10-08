a9dbed56ffe155057bf8aa4406382f260779d22f
Jane Amero <janie.amero@gmail.com>
2018-10-08 22:20:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,6 @@ function addElementToBeginningOfArray(){
   var newChoc = ['milky way', ...chocolateBars];
 }
 
-function destructivelyAddElementToBeginningOfArray(chocolateBars){
-  chocolateBars.unshift()
+function destructivelyAddElementToBeginningOfArray(x){
+  chocolateBars.unshift(x)
 }
