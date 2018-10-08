57403f1c11648723f877b77cce68962bd2ebac01
Jane Amero <janie.amero@gmail.com>
2018-10-08 23:03:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,11 +6,17 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(chocolateBars, 'x'){
-   hocolateBars2 = ['x', ...chocolateBars];
+  var chocolateBars = [
+    'snickers',
+    'hundred grand',
+    'kitkat',
+    'skittles'
+  ]
+   var chocolateBars2 = ['x', ...chocolateBars];
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, x){
-  chocolateBars = chocolateBars.unshift("foo");
+  chocolateBars.unshift("foo");
 }
 
 function addElementToEndOfArray(Array, Element){
